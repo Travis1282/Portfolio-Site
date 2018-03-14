6503a15bd472b73203358559e68cd354a8a86384
Travis1282 <tmail20@gmail.com>
2018-03-14 00:33:23 -0500

    using cookies and finding ip address

--- js/app.js
+++ js/app.js
@@ -1,68 +1,152 @@
-
 let		typeSpeed = 1, //in milliseconds
-		brouserWidth = window.innerWidth,
-		brouserHeight= window.innerHeight,
+		brouserWidth = (window.innerWidth-80)/9.6,
+		brouserHeight = (window.innerHeight-80)/20,
 		lineLength = 0, // keep track of printed text length on each line
 		numberOfLines = 0, // keep track of printed lines 
-		txt = '+'; // all text to be printed in the window 
+		typerCounter = 0, // for iterating over the text arrays
+		speed = 0,
+		waiting = false,
+		blink = true,
+		navagation = false;
+
+
+//////////////// GET THE IP ADDRESS OF THE VISITOR, ADD A COOKIE AND GET THEIR LAST LOGIN ////////////////
+
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        	retured = JSON.parse(xhr.responseText)
+        ip = retured.ip
+
+		today = new Date()
+		console.log(today)
+		today.setTime(today.getTime() + 31600000000);
+		console.log(today)
+
+		const expires = "; expires=" + today.toGMTString();
+		console.log(cookie = 'name	='+ ip + expires+";")
+       	document.cookie = 'name	='+ ip + expires+";";
+       	// savedCookie = document.cookie
+       	console.log(document.cookie)
+
+        draw();
+    }
+}
+
+xhr.open('GET', 'https://freegeoip.net/json/', true);
+xhr.send(null); 
+
+
+
 
 //////////////// MAINTAIN THE BROWSER WIDTH AND HEIGHT VARIBLES ON RESIZE ////////////////
 
-window.onresize = function(event) {
-  	brouserWidth = window.innerWidth;
-  	brouserHeight = window.innerHeight;
+window.onresize = (event) => {
+  	brouserWidth = (window.innerWidth-70)/10;
+  	brouserHeight = (window.innerHeight-70)/20;
   	console.log(brouserWidth + ' px width', brouserHeight + ' px height');
 };
 
-//////////////// CREATE THE MAN WINDOW ////////////////
 
-let terminalWindow = document.createElement('div');   // create main window 
+//////////////// DOM MANIPULATION ////////////////
+
+let terminalWindow = document.createElement('div');   // create main window container
 document.body.appendChild(terminalWindow);
-terminalWindow.id = 'terminal';
 terminalWindow.style.margin = '40px';
 terminalWindow.style.color = '#0bc';
-terminalWindow.style.transform = 'translate(0, 20px, 0 20px)' 
-terminalWindow.style.fontFamily = "'Courier New', Courier, monospace"
+terminalWindow.style.transform = 'translate(0, 20px, 0 20px)';
+
+const terminal = document.createElement('div'); // create terminal window where the text lives
+terminalWindow.appendChild(terminal);
+terminal.style.fontFamily = "'Courier New', Courier, monospace";
+terminal.style.color = 'white';
+terminal.style.zIndex = '1'
+terminal.style.position = 'relative';
+
+let blinker = document.createElement('div'); /// create the blinker for the text input area
+terminalWindow.appendChild(blinker);
+blinker.style.backgroundColor = '#0f0';
+blinker.style.width = '10px';
+blinker.style.height = '20px';
+blinker.style.marginTop = '-20px';
+blinker.style.position = 'relative';
 
 
-//////////////// SET THE TYPE IN AVAILALBE WINDOW ////////////////
+//////////////// SET TYPE IN AVAILALBE WINDOW ////////////////
 
-setInterval(typer, 1); // animation set with milliseconds
-
-function typer(txt){
-	if ((brouserWidth-80)/10 <= lineLength){ // inserts a return at the end of the window 
-	 	terminalWindow.innerText += "\n";
-	 	numberOfLines ++ ;
+const printChar = (text)=>{
+  	let textArray =  text.split('') 
+	if (Math.floor(brouserWidth) < lineLength){ // inserts a return at the end of the window 
+	 	terminal.innerText += '\n';
+	 	numberOfLines++ ;
 	 	lineLength = 0;
-	}else if((brouserHeight-80)/20 >= numberOfLines){ // adds text 
-		terminalWindow.innerText += txt;
-		lineLength ++ ; 
-	}else{
-		terminalWindow.innerText += '|';
-		blinker()
-	} 	 
+
+	}else if (textArray.length >= typerCounter){ // adds text 
+		if (textArray[typerCounter] == ' '){// handle spaces
+				terminal.innerText += '\u00A0'; 
+			}else if (textArray[typerCounter] != null ){// inputs text to dom 
+				terminal.innerText += textArray[typerCounter];
+				blinker.style.transform ='translateX('+ lineLength * 9.6 +'px)';
+
+			}else  { // end of input 
+					lineLength--
+				  	//blinker.style.transform ='translateX('+ lineLength * 9.6 +'px)'
+					waiting = true;
+			}
+			lineLength++ ; 
+			typerCounter++ ;
+	}
+
+  };
+
+
+ ////////////// CALCULATE NAVIGATION ////////////
+
+// const calcNav = () => {
+// 	let printableString = ''
+// 	let navCenter = Math.floor(brouserWidth)/2;
+// 	let spaceBetween = '' 
+// 	for (k = 0; navCenter >= k; k++){
+// 		spaceBetween += '.'
+// 		// \u00A0
+// 	}
+// 	console.log(spaceBetween)
+// 	for (i = 0; about.length-1 >= i; i++) {
+// 			console.log(about.length)
+// 			let navInstace = about[i]
+// 			let splitString = navInstace.split('')
+// 			let navCharLength = Math.ceil(navInstace.length / 5)
+// 			// console.log(navInstace)
+// 			//printableString += spaceBetween
+// 			for (j = 0; navInstace.length-1 >= j; j++) {
+// 				printableString += navInstace[j]
+// 				//console.log(printableString)
+// 			}
+// 	}	printableString += spaceBetween
+
+// 	console.log(printableString)
+// 	return printableString
+//   };
+// calcNav()
+
+//////////////// INITIALIZE INTODUCTORY WINDOW MESSAGE ////////////////
+
+function draw() {
+    setTimeout(function() {
+        requestAnimationFrame(draw);
+		if (waiting == false) printChar(intro);
+			else { 	
+			speed = 500;
+			blinker.style.visibility = (blink = !blink) ? 'hidden' :  'visible' ; // blink the cursor 
+			
+		}
+
+    }, speed);
 }
+ 
 
 
-//////////////// BLINKING CURSOR IF NOT CURRENTLY TYPING  ////////////////
 
-function blinker(){
-	// if ( ) 
-
-}
-
-// document.getElementById("terminal").addEventListener("click", function(){
-//         var myElement = document.getElementById('text-element');
-//         var startPosition = myElement.selectionStart;
-//         var endPosition = myElement.selectionEnd;
-        
-//         // Check if you've selected text
-//         if(startPosition == endPosition){
-//             console.log("The position of the cursor is (" + startPosition + "/" + myElement.value.length + ")");
-//         }else{
-//             console.log("Selected text from ("+ startPosition +" to "+ endPosition + " of " + myElement.value.length + ")");
-//         }
-//     },false);
 
 
 
---
+++ js/introText.js
@@ -0,0 +1,45 @@
+const intro = "Hello My name is Travis Clark. I'm a web engineer based in Chicago Il."
+
+
+//                       
+//  /\  |_   _     _|_  
+// /--\ |_) (_) |_| |_  
+//                       
+//   (type "about")      
+//21 charicters wide 
+const aboutWidth = 21 
+const about = "                       &  /\  |_   _     _|_   & /--\ |_) (_) |_| |_   &                       &     (type about)      &"
+
+
+//  _                       
+// /   _  ._ _|_  _.  _ _|_ 
+// \_ (_) | | |_ (_| (_  |_ 
+//
+//       (type contact)      
+//26 charicters wide                          
+const contactWidth = 26
+const contact = "  _                       & /   _  ._ _|_  _.  _ _|_ & \_ (_) | | |_ (_| (_  |_ &                          &      (type contact)      &"
+
+
+
+//  _                        
+// |_) ._ _  o  _   _ _|_  _ 
+// |   | (_) | (/_ (_  |_ _> 
+//          _|               
+//     (type projects)       
+
+const projectstWidth = 27
+const projects = "  _                        & |_) ._ _  o  _   _ _|_  _ & |   | (_) | (/_ (_  |_ _> &          _|               &     (type projects)       &"
+
+
+// ___             &
+//  |  _   _  |  _ &
+//  | (_) (_) | _> &                
+//                 &
+//  (type tools)   &   
+const toolsWidth = 16
+const tools =" ___             &  |  _   _  |  _ &  | (_) (_) | _> &                                 &  (type tools)   & "
+
+
+nav = [projects, tools, about, contact];
+
